8720db18002e2c7af628ed82d3df7a22c409e067
Linfeng Qian <thewawar@gmail.com>
2022-02-25 14:17:40 +0800

    refactor: remove TxHelper and MockTransactionHelper

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 mod chain;
 mod error;
-#[allow(clippy::mutable_key_type)]
-mod transaction;
-#[allow(clippy::mutable_key_type)]
-mod tx_helper;
 mod types;
 mod util;
 
@@ -15,11 +11,6 @@ pub mod rpc;
 pub use chain::{calc_max_mature_number, GenesisInfo};
 pub use error::Error;
 pub use rpc::HttpRpcClient;
-pub use transaction::{
-    MockCellDep, MockInfo, MockInput, MockResourceLoader, MockTransaction, MockTransactionHelper,
-    ReprMockCellDep, ReprMockInfo, ReprMockInput, ReprMockTransaction,
-};
-pub use tx_helper::{build_signature, MultisigConfig, SignerFn, TxHelper};
 pub use types::{
     Address, AddressPayload, AddressType, CodeHashIndex, HumanCapacity, NetworkType, OldAddress,
     OldAddressFormat, Since, SinceType,
