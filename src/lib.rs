72fe2b0d629b373d5dcc381b4c828488cf66a89d
Linfeng Qian <thewawar@gmail.com>
2022-04-19 16:08:15 +0800

    chore: make more types public in root

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub use chain::{calc_max_mature_number, GenesisInfo, ParseGenesisInfoError};
 pub use rpc::{CkbRpcClient, IndexerRpcClient, RpcError};
 pub use types::{
     Address, AddressPayload, AddressType, CodeHashIndex, HumanCapacity, NetworkType, OldAddress,
-    OldAddressFormat, Since, SinceType,
+    OldAddressFormat, ScriptGroup, ScriptGroupType, ScriptId, Since, SinceType,
 };
 
 pub use ckb_crypto::secp::SECP256K1;
