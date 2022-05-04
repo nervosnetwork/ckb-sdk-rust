004e5bc2a7372a5fc0cf3a050f5dcf8c6be4ed32
Linfeng Qian <thewawar@gmail.com>
2022-05-04 14:55:06 +0800

    test: move sighash unlocker test to standalone module

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,10 @@ pub mod util;
 #[cfg(feature = "test")]
 pub mod test_util;
 
+#[cfg(feature = "test")]
+#[cfg(test)]
+mod tests;
+
 pub use chain::{calc_max_mature_number, GenesisInfo, ParseGenesisInfoError};
 pub use rpc::{CkbRpcClient, IndexerRpcClient, RpcError};
 pub use types::{
