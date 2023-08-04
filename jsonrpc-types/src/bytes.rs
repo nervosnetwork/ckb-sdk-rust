16e1c9de92749f1d5e285d8cb0191afd1313207e
EthanYuan <yuanchengxing@hotmail.com>
2023-08-04 10:11:34 +0800

    Copy and paste jsonrpc-types from ckb.

---
+++ jsonrpc-types/src/bytes.rs
@@ -0,0 +1,129 @@
+use ckb_types::{bytes::Bytes, packed, prelude::*};
+use faster_hex::{hex_decode, hex_encode};
+use std::fmt;
+
+/// Variable-length binary encoded as a 0x-prefixed hex string in JSON.
+///
+/// ## Example
+///
+/// | JSON       | Binary                               |
+/// | ---------- | ------------------------------------ |
+/// | "0x"       | Empty binary                         |
+/// | "0x00"     | Single byte 0                        |
+/// | "0x636b62" | 3 bytes, UTF-8 encoding of ckb       |
+/// | "00"       | Invalid, 0x is required              |
+/// | "0x0"      | Invalid, each byte requires 2 digits |
+#[derive(Clone, PartialEq, Eq, Hash, Debug, Default)]
+pub struct JsonBytes(Bytes);
+
+impl JsonBytes {
+    /// Creates the `JsonBytes` from `Bytes`.
+    pub fn from_bytes(bytes: Bytes) -> Self {
+        JsonBytes(bytes)
+    }
+
+    /// Creates the `JsonBytes` from `Vec<u8>`.
+    pub fn from_vec(vec: Vec<u8>) -> Self {
+        JsonBytes(Bytes::from(vec))
+    }
+
+    /// Converts into `Bytes`.
+    pub fn into_bytes(self) -> Bytes {
+        let JsonBytes(bytes) = self;
+        bytes
+    }
+
+    /// Gets the number of bytes.
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Tells whether this is an empty bytes.
+    pub fn is_empty(&self) -> bool {
+        0 == self.len()
+    }
+
+    /// Gets the underlying slice.
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl From<packed::Bytes> for JsonBytes {
+    fn from(input: packed::Bytes) -> Self {
+        JsonBytes::from_bytes(input.raw_data())
+    }
+}
+
+impl<'a> From<&'a packed::Bytes> for JsonBytes {
+    fn from(input: &'a packed::Bytes) -> Self {
+        JsonBytes::from_bytes(input.raw_data())
+    }
+}
+
+impl From<JsonBytes> for packed::Bytes {
+    fn from(input: JsonBytes) -> Self {
+        input.0.pack()
+    }
+}
+
+struct BytesVisitor;
+
+impl<'b> serde::de::Visitor<'b> for BytesVisitor {
+    type Value = JsonBytes;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a 0x-prefixed hex string")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        if v.len() < 2 || &v.as_bytes()[0..2] != b"0x" {
+            return Err(E::invalid_value(serde::de::Unexpected::Str(v), &self));
+        }
+
+        if v.len() & 1 != 0 {
+            return Err(E::invalid_length(v.len(), &"even length"));
+        }
+
+        let bytes = &v.as_bytes()[2..];
+        if bytes.is_empty() {
+            return Ok(JsonBytes::default());
+        }
+        let mut buffer = vec![0; bytes.len() >> 1]; // we checked length
+        hex_decode(bytes, &mut buffer).map_err(|e| E::custom(format_args!("{e:?}")))?;
+        Ok(JsonBytes::from_vec(buffer))
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        self.visit_str(&v)
+    }
+}
+
+impl serde::Serialize for JsonBytes {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut buffer = vec![0u8; self.len() * 2 + 2];
+        buffer[0] = b'0';
+        buffer[1] = b'x';
+        hex_encode(self.as_bytes(), &mut buffer[2..])
+            .map_err(|e| serde::ser::Error::custom(format!("{e}")))?;
+        serializer.serialize_str(unsafe { ::std::str::from_utf8_unchecked(&buffer) })
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for JsonBytes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(BytesVisitor)
+    }
+}
---
+++ jsonrpc-types/src/cell.rs
@@ -0,0 +1,147 @@
+use crate::{CellOutput, JsonBytes};
+use ckb_types::{
+    core::cell::{CellMeta, CellStatus},
+    prelude::Unpack,
+    H256,
+};
+use serde::{Deserialize, Serialize};
+
+/// The JSON view of a cell with its status information.
+///
+/// ## Examples
+///
+/// ```
+/// # serde_json::from_str::<ckb_jsonrpc_types::CellWithStatus>(r#"
+/// {
+///   "cell": {
+///     "data": {
+///       "content": "0x7f454c460201010000000000000000000200f3000100000078000100000000004000000000000000980000000000000005000000400038000100400003000200010000000500000000000000000000000000010000000000000001000000000082000000000000008200000000000000001000000000000001459308d00573000000002e7368737472746162002e74657874000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b000000010000000600000000000000780001000000000078000000000000000a0000000000000000000000000000000200000000000000000000000000000001000000030000000000000000000000000000000000000082000000000000001100000000000000000000000000000001000000000000000000000000000000",
+///       "hash": "0x28e83a1277d48add8e72fadaa9248559e1b632bab2bd60b27955ebc4c03800a5"
+///     },
+///     "output": {
+///       "capacity": "0x802665800",
+///       "lock": {
+///         "args": "0x",
+///         "code_hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+///         "hash_type": "data"
+///       },
+///       "type": null
+///     }
+///   },
+///   "status": "live"
+/// }
+/// # "#).unwrap();
+/// ```
+///
+/// ```
+/// # serde_json::from_str::<ckb_jsonrpc_types::CellWithStatus>(r#"
+/// {
+///   "cell": null,
+///   "status": "unknown"
+/// }
+/// # "#).unwrap();
+/// ```
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CellWithStatus {
+    /// The cell information.
+    ///
+    /// For performance issues, CKB only keeps the information for live cells.
+    pub cell: Option<CellInfo>,
+    /// Status of the cell.
+    ///
+    /// Allowed values: "live", "dead", "unknown".
+    ///
+    /// * `live` - The transaction creating this cell is in the chain, and there are no
+    /// transactions found in the chain that uses this cell as an input.
+    /// * `dead` - (**Deprecated**: the dead status will be removed since 0.36.0, please do not
+    /// rely on the logic that differentiates dead and unknown cells.) The transaction creating
+    /// this cell is in the chain, and a transaction is found in the chain which uses this cell as
+    /// an input.
+    /// * `unknown` - CKB does not know the status of the cell. Either the transaction creating
+    /// this cell is not in the chain yet, or it is no longer live.
+    pub status: String,
+}
+
+/// The JSON view of a cell combining the fields in cell output and cell data.
+///
+/// ## Examples
+///
+/// ```
+/// # serde_json::from_str::<ckb_jsonrpc_types::CellInfo>(r#"
+/// {
+///   "data": {
+///     "content": "0x7f454c460201010000000000000000000200f3000100000078000100000000004000000000000000980000000000000005000000400038000100400003000200010000000500000000000000000000000000010000000000000001000000000082000000000000008200000000000000001000000000000001459308d00573000000002e7368737472746162002e74657874000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b000000010000000600000000000000780001000000000078000000000000000a0000000000000000000000000000000200000000000000000000000000000001000000030000000000000000000000000000000000000082000000000000001100000000000000000000000000000001000000000000000000000000000000",
+///     "hash": "0x28e83a1277d48add8e72fadaa9248559e1b632bab2bd60b27955ebc4c03800a5"
+///   },
+///   "output": {
+///     "capacity": "0x802665800",
+///     "lock": {
+///       "args": "0x",
+///       "code_hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+///       "hash_type": "data"
+///     },
+///     "type": null
+///   }
+/// }
+/// # "#).unwrap();
+/// ```
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CellInfo {
+    /// Cell fields appears in the transaction `outputs` array.
+    pub output: CellOutput,
+    /// Cell data.
+    ///
+    /// This is `null` when the data is not requested, which does not mean the cell data is empty.
+    pub data: Option<CellData>,
+}
+
+/// The cell data content and hash.
+///
+/// ## Examples
+///
+/// ```
+/// # serde_json::from_str::<ckb_jsonrpc_types::CellData>(r#"
+/// {
+///   "content": "0x7f454c460201010000000000000000000200f3000100000078000100000000004000000000000000980000000000000005000000400038000100400003000200010000000500000000000000000000000000010000000000000001000000000082000000000000008200000000000000001000000000000001459308d00573000000002e7368737472746162002e74657874000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b000000010000000600000000000000780001000000000078000000000000000a0000000000000000000000000000000200000000000000000000000000000001000000030000000000000000000000000000000000000082000000000000001100000000000000000000000000000001000000000000000000000000000000",
+///   "hash": "0x28e83a1277d48add8e72fadaa9248559e1b632bab2bd60b27955ebc4c03800a5"
+/// }
+/// # "#).unwrap();
+/// ```
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CellData {
+    /// Cell content.
+    pub content: JsonBytes,
+    /// Cell content hash.
+    pub hash: H256,
+}
+
+impl From<CellMeta> for CellInfo {
+    fn from(cell_meta: CellMeta) -> Self {
+        let data = cell_meta.mem_cell_data;
+        let data_hash = cell_meta.mem_cell_data_hash;
+        let output = cell_meta.cell_output.into();
+        CellInfo {
+            output,
+            data: data.and_then(move |data| {
+                data_hash.map(|hash| CellData {
+                    content: JsonBytes::from_bytes(data),
+                    hash: hash.unpack(),
+                })
+            }),
+        }
+    }
+}
+
+impl From<CellStatus> for CellWithStatus {
+    fn from(status: CellStatus) -> Self {
+        let (cell, status) = match status {
+            CellStatus::Live(cell_meta) => (Some(cell_meta), "live"),
+            CellStatus::Dead => (None, "dead"),
+            CellStatus::Unknown => (None, "unknown"),
+        };
+        Self {
+            cell: cell.map(Into::into),
+            status: status.to_string(),
+        }
+    }
+}
